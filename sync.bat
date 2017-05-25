38bcc4000bfc9ad303c6d84e64773c3830602e27
J Sanders <j@silvershade.net>
2017-05-25 17:17:29 -0500

    Create sync.bat

---
+++ sync.bat
@@ -0,0 +1,6 @@
+@echo off
+
+xcopy "C:\Documents and Settings\venrxm1\My Documents"
+"F:\Documents\Employment\BBVA" /E /I /S /Y
+
+xcopy "F:\Documents\Employment\BBVA" "C:\Documents and Settings\venrxm1\My Documents" /E /I /S /Y
